9c67cb135b4fbedbec5e12178429f54832fe2d0f
ToliTorres <toliluz@gmail.com>
2026-05-30 16:56:58 -0600

    Add categories page and improve styling

--- server.js
+++ server.js
@@ -30,26 +30,24 @@ app.set('views', path.join(__dirname, 'src/views'));
  * Routes
  */
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     const title = 'Home';
-
     res.render('home', { title });
 });
 
-app.get('/organizations', (req, res) => {
-
+app.get('/organizations', async (req, res) => {
     const title = 'Our Partner Organizations';
-
     res.render('organizations', { title });
-
 });
 
-app.get('/projects', (req, res) => {
-
+app.get('/projects', async (req, res) => {
     const title = 'Service Projects';
-
     res.render('projects', { title });
+});
 
+app.get('/categories', async (req, res) => {
+    const title = 'Service Categories';
+    res.render('categories', { title });
 });
 
 app.listen(PORT, () => {
